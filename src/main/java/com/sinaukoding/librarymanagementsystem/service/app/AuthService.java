79347c5a563c4216b4a1a6f0fe7748d4c0f61784
project-java-reza <rizqirezaardiansyahjava@gmail.com>
2025-09-11 17:48:31 +0700

    [FEATURE]  Create table login m_user and join column to table m_role

--- src/main/java/com/sinaukoding/librarymanagementsystem/service/app/AuthService.java
+++ src/main/java/com/sinaukoding/librarymanagementsystem/service/app/AuthService.java
@@ -5,12 +5,15 @@ import com.sinaukoding.librarymanagementsystem.entity.managementuser.User;
 import com.sinaukoding.librarymanagementsystem.model.app.SimpleMap;
 import com.sinaukoding.librarymanagementsystem.model.request.AdminRequestRecord;
 import com.sinaukoding.librarymanagementsystem.model.request.LoginRequestRecord;
+import com.sinaukoding.librarymanagementsystem.model.request.UserRequestRecord;
 
 public interface AuthService {
 
     SimpleMap login(LoginRequestRecord request);
 
     void registerAdmin(AdminRequestRecord request);
+
+    void registerUser(UserRequestRecord request);
 
     void logout(User userLoggedIn);
 
