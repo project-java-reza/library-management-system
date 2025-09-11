1dc1ef261512755e7f5d39f2d275f8054011884f
project-java-reza <rizqirezaardiansyahjava@gmail.com>
2025-09-11 11:07:34 +0700

    [IMPROVE] First Commit

---
+++ src/test/java/com/sinaukoding/librarymanagementsystem/AplikasiLibraryManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sinaukoding.librarymanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AplikasiLibraryManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
