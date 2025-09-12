20f696155b18613dd82344372044e788f659f2b2
project-java-reza <rizqirezaardiansyahjava@gmail.com>
2025-09-13 06:05:08 +0700

    [FEATURE] add user role for system login

--- src/main/java/com/sinaukoding/librarymanagementsystem/config/UserLoggedInConfig.java
+++ src/main/java/com/sinaukoding/librarymanagementsystem/config/UserLoggedInConfig.java
@@ -1,36 +1,78 @@
 package com.sinaukoding.librarymanagementsystem.config;
 
 import com.sinaukoding.librarymanagementsystem.entity.managementuser.Admin;
+import com.sinaukoding.librarymanagementsystem.entity.managementuser.User;
 import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 @Getter
+@Slf4j
 public class UserLoggedInConfig implements UserDetails {
 
     private final Admin admin;
+    private final User user;
 
     public UserLoggedInConfig(Admin admin) {
         this.admin = admin;
+        this.user = null;
+    }
+
+    public UserLoggedInConfig(User user) {
+        this.user = user;
+        this.admin = null;
     }
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of(new SimpleGrantedAuthority("ROLE_" + admin.getRole().getRole().name()));
+        if (admin != null) {
+            return List.of(new SimpleGrantedAuthority("ROLE_" + admin.getRole().getRole().name()));  // Role Admin
+        }
+        else if (user != null) {
+            return List.of(new SimpleGrantedAuthority("ROLE_" + user.getRole().getRole().name()));  // Role User
+        }
+        return Collections.emptyList();
     }
 
     @Override
     public String getPassword() {
-        return admin.getPassword();
+        if (admin != null) {
+            return admin.getPassword();
+        } else {
+            assert user != null;
+            return user.getPassword();
+        }
     }
 
     @Override
     public String getUsername() {
-        return admin.getUsername();
+        if (admin != null) {
+            return admin.getUsername();
+        } else if (user != null){
+            return user.getUsername();
+        }
+        return null;
     }
+
+    public String getId() {
+        if (admin != null) {
+            log.info("Admin ID: " + admin.getId());
+            return admin.getId();
+        } else if (user != null) {
+            log.info("User ID: " + user.getId());
+            return user.getId();
+        }
+        log.error("Tidak ada Admin atau User yang ditemukan.");
+        return null;
+    }
+
+
+
 
 }
